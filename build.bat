f53a052d7a42468a0b0f8b975309d3575e3cf661
silent1603 <tamgiangbui@gmail.com>
2022-05-11 23:13:13 +0700

    update build.bat

--- build.bat
+++ build.bat
@@ -1,11 +1,18 @@
 @echo off
 IF NOT EXIST build (
     mkdir build 
-)
+) 
+
+
+git checkout --recurse-submodules
 git submodule update --init --recursive --remote
 IF NOT EXIST vcpkg/vcpkg.exe (
     echo install vcpkg
     ./vcpkg/bootstrap-vcpkg.bat
+    if %errorlevel%==1 (
+        del ./vcpkg/vcpkg.exe
+        ./vcpkg/bootstrap-vcpkg.bat
+    )
 )
 cd build 
 cmake ..
